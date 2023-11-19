bffb25c73aae18be04055a49f1f1b42e819ad617
Ali Hadi Elchab <alielchab01@gmail.com>
2023-11-19 11:08:47 +0200

    Set files structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import MovieDescription from "./pages/MovieDescription";
+import MoviesList from "./pages/MoviesList";
+import "./styles/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Netflix</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/movieslist" element={<MoviesList />} />
+        <Route path="/moviesdescription" element={<MovieDescription />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

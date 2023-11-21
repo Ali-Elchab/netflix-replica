ebc29cca75965b46075edc4f17e9dfee94d2e8f5
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-11-21 22:12:39 +0200

    All Together

--- src/pages/MovieDescription.jsx
+++ src/pages/MovieDescription.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import MovieDetails from "../components/MovieDetails";
+import AdditionalContent from "../components/AdditionalContent";
+import MoreLikeThis from "../components/MoreLikeThis";
+import ComingSoonSection from "../components/ComingSoon";
+import "../styles/Movie-Description.css";
 
 const MovieDescription = () => {
   return (
-    <div>MovieDescription</div>
-  )
-}
+    <div className="Movie-Description-Container">
+      <MovieDetails />
+      <AdditionalContent />
+      <MoreLikeThis />
+      <ComingSoonSection />
+    </div>
+  );
+};
 
-export default MovieDescription
+export default MovieDescription;
